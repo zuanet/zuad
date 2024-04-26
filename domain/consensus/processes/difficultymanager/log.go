e83dabc27007349c09c2a3982bc03db49eb54edc
zuanet <168155305+zuanet@users.noreply.github.com>
2024-04-26 08:54:10 +0300

    Add files via upload

---
+++ domain/consensus/processes/difficultymanager/log.go
@@ -0,0 +1,7 @@
+package difficultymanager
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+)
+
+var log = logger.RegisterSubSystem("DAA")
---
+++ domain/consensus/processes/finalitymanager/log.go
@@ -0,0 +1,7 @@
+package finalitymanager
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+)
+
+var log = logger.RegisterSubSystem("BDAG")
