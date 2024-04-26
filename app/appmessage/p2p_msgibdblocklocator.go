023188cde0fcc030808103857ee3ab5c73b1e1c1
zuanet <168155305+zuanet@users.noreply.github.com>
2024-04-26 07:57:58 +0300

    Add files via upload

---
+++ app/appmessage/p2p_msgibdblocklocator.go
@@ -0,0 +1,27 @@
+package appmessage
+
+import (
+	"github.com/zuanet/zuad/domain/consensus/model/externalapi"
+)
+
+// MsgIBDBlockLocator represents a zua ibdBlockLocator message
+type MsgIBDBlockLocator struct {
+	baseMessage
+	TargetHash         *externalapi.DomainHash
+	BlockLocatorHashes []*externalapi.DomainHash
+}
+
+// Command returns the protocol command string for the message
+func (msg *MsgIBDBlockLocator) Command() MessageCommand {
+	return CmdIBDBlockLocator
+}
+
+// NewMsgIBDBlockLocator returns a new zua ibdBlockLocator message
+func NewMsgIBDBlockLocator(targetHash *externalapi.DomainHash,
+	blockLocatorHashes []*externalapi.DomainHash) *MsgIBDBlockLocator {
+
+	return &MsgIBDBlockLocator{
+		TargetHash:         targetHash,
+		BlockLocatorHashes: blockLocatorHashes,
+	}
+}
