8a6d3c6781c1d1a372b773d05ba6d7243f3a084a
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-05-18 17:05:22 +0700

    Update genesis.go

--- domain/dagconfig/genesis.go
+++ domain/dagconfig/genesis.go
@@ -62,8 +62,10 @@ func main() {
             0,
             []externalapi.BlockLevelParents{},
             genesisMerkleRoot,
-            externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{}),
-            externalapi.NewDomainHashFromByteArray(muhash.EmptyMuHashHash.AsArray()),
+	    &externalapi.DomainHash{},
+            externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
+		0x71, 0x0f, 0x27, 0xdf, 0x42, 0x3e, 0x63, 0xaa, 0x6c, 0xdb, 0x72, 0xb8, 0x9e, 0xa5, 0xa0, 0x6c, 0xff, 0xa3, 0x99, 0xd6, 0x6f, 0x16, 0x77, 0x04, 0x45, 0x5b, 0x5a, 0xf5, 0x9d, 0xef, 0x8e, 0x20,
+             }),		    
             uint64(currentTime),
             1716025866901,
             0x3392c,
@@ -74,7 +76,6 @@ func main() {
         ),
         Transactions: []*externalapi.DomainTransaction{genesisCoinbaseTx},
     }
-
 var devnetGenesisTxOuts = []*externalapi.DomainTransactionOutput{}
 
 var devnetGenesisTxPayload = []byte{
@@ -83,17 +84,23 @@ var devnetGenesisTxPayload = []byte{
 	0x00, 0x00, // Script version
 	0x01,                                                                   // Varint
 	0x00,                                                                   // OP-FALSE
-	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x64, 0x65, 0x76, 0x6e, 0x65, 0x74, 
+	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x64, 0x65, 0x76, 0x6e, 0x65, 0x74, // kaspa-devnet
 }
 
+// devnetGenesisCoinbaseTx is the coinbase transaction for the genesis blocks for
+// the development network.
 var devnetGenesisCoinbaseTx = transactionhelper.NewSubnetworkTransaction(0,
 	[]*externalapi.DomainTransactionInput{}, devnetGenesisTxOuts,
 	&subnetworks.SubnetworkIDCoinbase, 0, devnetGenesisTxPayload)
 
+// devGenesisHash is the hash of the first block in the block DAG for the development
+// network (genesis block).
 var devnetGenesisHash = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0xb3, 0x13, 0x87, 0x0a, 0x32, 0xc7, 0x04, 0xbd, 0xf1, 0x21, 0x4a, 0x3b, 0x27, 0x0c, 0xc4, 0x75, 0xd9, 0x42, 0xc2, 0x09, 0x2d, 0x37, 0x9b, 0xc8, 0x70, 0x0a, 0xb0, 0x43, 0x31, 0x9e, 0xf8, 0x46,
 })
 
+// devnetGenesisMerkleRoot is the hash of the first transaction in the genesis block
+// for the devopment network.
 var devnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0x58, 0xab, 0xf2, 0x03, 0x21, 0xd7, 0x07, 0x16,
 	0x16, 0x2b, 0x6b, 0xf8, 0xd9, 0xf5, 0x89, 0xca,
@@ -101,6 +108,8 @@ var devnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[externala
 	0x7f, 0xa6, 0x5d, 0x11, 0x41, 0xa3, 0xf9, 0x4d,
 })
 
+// devnetGenesisBlock defines the genesis block of the block DAG which serves as the
+// public transaction ledger for the development network.
 var devnetGenesisBlock = externalapi.DomainBlock{
 	Header: blockheader.NewImmutableBlockHeader(
 		0,
@@ -127,13 +136,16 @@ var simnetGenesisTxPayload = []byte{
 	0x00, 0x00, // Script version
 	0x01,                                                                   // Varint
 	0x00,                                                                   // OP-FALSE
-	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x73, 0x69, 0x6d, 0x6e, 0x65, 0x74, 
+	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x73, 0x69, 0x6d, 0x6e, 0x65, 0x74, // kaspa-simnet
 }
 
+// simnetGenesisCoinbaseTx is the coinbase transaction for the simnet genesis block.
 var simnetGenesisCoinbaseTx = transactionhelper.NewSubnetworkTransaction(0,
 	[]*externalapi.DomainTransactionInput{}, simnetGenesisTxOuts,
 	&subnetworks.SubnetworkIDCoinbase, 0, simnetGenesisTxPayload)
 
+// simnetGenesisHash is the hash of the first block in the block DAG for
+// the simnet (genesis block).
 var simnetGenesisHash = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0x41, 0x1f, 0x8c, 0xd2, 0x6f, 0x3d, 0x41, 0xae,
 	0xa3, 0x9e, 0x78, 0x57, 0x39, 0x27, 0xda, 0x24,
@@ -141,6 +153,8 @@ var simnetGenesisHash = externalapi.NewDomainHashFromByteArray(&[externalapi.Dom
 	0x95, 0x9b, 0x91, 0x27, 0xe9, 0x6b, 0x79, 0xe3,
 })
 
+// simnetGenesisMerkleRoot is the hash of the first transaction in the genesis block
+// for the devopment network.
 var simnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0x19, 0x46, 0xd6, 0x29, 0xf7, 0xe9, 0x22, 0xa7,
 	0xbc, 0xed, 0x59, 0x19, 0x05, 0x21, 0xc3, 0x77,
@@ -148,6 +162,8 @@ var simnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[externala
 	0x56, 0x4a, 0xd7, 0xfd, 0x56, 0x85, 0x7c, 0x1b,
 })
 
+// simnetGenesisBlock defines the genesis block of the block DAG which serves as the
+// public transaction ledger for the development network.
 var simnetGenesisBlock = externalapi.DomainBlock{
 	Header: blockheader.NewImmutableBlockHeader(
 		0,
@@ -174,13 +190,16 @@ var testnetGenesisTxPayload = []byte{
 	0x00, 0x00, // Script version
 	0x01,                                                                         // Varint
 	0x00,                                                                         // OP-FALSE
-	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x74, 0x65, 0x73, 0x74, 0x6e, 0x65, 0x74, 
+	0x6b, 0x61, 0x73, 0x70, 0x61, 0x2d, 0x74, 0x65, 0x73, 0x74, 0x6e, 0x65, 0x74, // kaspa-testnet
 }
 
+// testnetGenesisCoinbaseTx is the coinbase transaction for the testnet genesis block.
 var testnetGenesisCoinbaseTx = transactionhelper.NewSubnetworkTransaction(0,
 	[]*externalapi.DomainTransactionInput{}, testnetGenesisTxOuts,
 	&subnetworks.SubnetworkIDCoinbase, 0, testnetGenesisTxPayload)
 
+// testnetGenesisHash is the hash of the first block in the block DAG for the test
+// network (genesis block).
 var testnetGenesisHash = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0xf8, 0x96, 0xa3, 0x03, 0x48, 0x73, 0xbe, 0x17,
 	0x39, 0xfc, 0x43, 0x59, 0x23, 0x68, 0x99, 0xfd,
@@ -188,6 +207,8 @@ var testnetGenesisHash = externalapi.NewDomainHashFromByteArray(&[externalapi.Do
 	0xf0, 0xd0, 0xda, 0x3e, 0xb1, 0xcc, 0x43, 0x70,
 })
 
+// testnetGenesisMerkleRoot is the hash of the first transaction in the genesis block
+// for testnet.
 var testnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[externalapi.DomainHashSize]byte{
 	0x17, 0x34, 0x14, 0x08, 0xa5, 0x72, 0x45, 0x56,
 	0x50, 0x4d, 0xf4, 0xd6, 0xcf, 0x51, 0x5c, 0xbf,
@@ -195,6 +216,8 @@ var testnetGenesisMerkleRoot = externalapi.NewDomainHashFromByteArray(&[external
 	0x3c, 0x22, 0xd5, 0xe9, 0x11, 0x72, 0x0c, 0x2a,
 })
 
+// testnetGenesisBlock defines the genesis block of the block DAG which serves as the
+// public transaction ledger for testnet.
 var testnetGenesisBlock = externalapi.DomainBlock{
 	Header: blockheader.NewImmutableBlockHeader(
 		0,
