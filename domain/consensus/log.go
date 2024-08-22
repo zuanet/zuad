e8770d71470216672afbb379ac0d58600a45bf5a
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 00:59:11 +0300

    Delete domain/consensus/log.go

--- domain/consensus/log.go
+++
@@ -1,9 +0,0 @@
-package consensus
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("BDAG")
-var spawn = panics.ZuaoutineWrapperFunc(log)
