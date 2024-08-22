cb17ebdd051b94b434530eddb7e8ef5980a6d9ff
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 00:34:52 +0300

    Delete app/protocol/flows directory

--- app/protocol/flows/ready/log.go
+++
@@ -1,9 +0,0 @@
-package ready
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("PROT")
-var spawn = panics.ZuaoutineWrapperFunc(log)
--- app/protocol/flows/v5/blockrelay/log.go
+++
@@ -1,9 +0,0 @@
-package blockrelay
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("PROT")
-var spawn = panics.ZuaoutineWrapperFunc(log)
