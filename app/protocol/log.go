d770c2b81a91fc12b1c0f3ae958bb67633f4274c
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 00:39:28 +0300

    Add files via upload

---
+++ app/protocol/log.go
@@ -0,0 +1,9 @@
+package protocol
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var log = logger.RegisterSubSystem("PROT")
+var spawn = panics.GoroutineWrapperFunc(log)
