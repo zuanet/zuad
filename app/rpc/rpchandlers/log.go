8d45f5ff499ec277da18167c273fea82ef46c5d2
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 00:46:28 +0300

    Add files via upload

---
+++ app/rpc/rpchandlers/log.go
@@ -0,0 +1,9 @@
+package rpchandlers
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var log = logger.RegisterSubSystem("RPCS")
+var spawn = panics.GoroutineWrapperFunc(log)
