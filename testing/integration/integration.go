17c6b2ff252bdd27e3b41f30bd00f6ddba6db1fb
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 02:22:06 +0300

    Delete testing/integration directory

--- testing/integration/integration.go
+++
@@ -1,4 +0,0 @@
-package integration
-
-// Because of a bug in Go coverage fails if you have packages with test files only. See https://github.com/golang/go/issues/27333
-// So this is a dummy non-test go file in the package.
