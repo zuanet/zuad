8b018216844edf080ada96e940910232c04d047d
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 01:31:25 +0300

    Add files via upload

---
+++ infrastructure/network/connmanager/log.go
@@ -0,0 +1,9 @@
+package connmanager
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var log = logger.RegisterSubSystem("CMGR")
+var spawn = panics.GoroutineWrapperFunc(log)
