b6c68d7cf9637fba6c6a23956b4190f18e6afef8
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 01:36:23 +0300

    Delete infrastructure/network/netadapter/standalone/log.go

--- infrastructure/network/netadapter/standalone/log.go
+++
@@ -1,9 +0,0 @@
-package standalone
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("NTAR")
-var spawn = panics.ZuaoutineWrapperFunc(log)
