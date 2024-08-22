9239709ce09c82a7f2c0716aca9fec2e49161aea
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 00:59:37 +0300

    Add files via upload

---
+++ domain/consensus/log.go
@@ -0,0 +1,9 @@
+package consensus
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var log = logger.RegisterSubSystem("BDAG")
+var spawn = panics.GoroutineWrapperFunc(log)
