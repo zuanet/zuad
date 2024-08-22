1a82a2594128675ce30894fd087112951f715b52
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 01:46:51 +0300

    Delete stability-tests/common directory

--- stability-tests/common/paths.go
+++
@@ -1,6 +0,0 @@
-package common
-
-import "github.com/zuanet/zuad/util"
-
-// DefaultAppDir is the default app directory to be used by all tests
-var DefaultAppDir = util.AppDir("stability-tests", false)
