2a966477e152ca3c63e1ed919803ea1f4c0b111d
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 01:30:47 +0300

    Delete infrastructure/network/connmanager directory

--- infrastructure/network/connmanager/log.go
+++
@@ -1,9 +0,0 @@
-package connmanager
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("CMGR")
-var spawn = panics.ZuaoutineWrapperFunc(log)
