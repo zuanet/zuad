6ad1bd2f3db6cbda42bcdac63b850a9cf414372d
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 01:25:16 +0300

    Delete domain/prefixmanager/log.go

--- domain/prefixmanager/log.go
+++
@@ -1,9 +0,0 @@
-package prefixmanager
-
-import (
-	"github.com/zuanet/zuad/infrastructure/logger"
-	"github.com/zuanet/zuad/util/panics"
-)
-
-var log = logger.RegisterSubSystem("PRFX")
-var spawn = panics.ZuaoutineWrapperFunc(log)
