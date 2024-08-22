1bfe8a2777e79b1cdd5f2f1cb94b26df21622837
zuanet <168155305+zuanet@users.noreply.github.com>
2024-08-23 02:14:32 +0300

    Add files via upload

---
+++ stability-tests/orphans/log.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var (
+	backendLog = logger.NewBackend()
+	log        = backendLog.Logger("ORPH")
+	spawn      = panics.GoroutineWrapperFunc(log)
+)
