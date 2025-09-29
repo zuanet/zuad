fc26978fedb2fc50b4a013097e7866b79a7421c0
nacho <49112849+nacshaa@users.noreply.github.com>
2025-09-29 19:46:28 +0700

    Update transactionvalidator.go

--- domain/consensus/processes/transactionvalidator/transactionvalidator.go
+++ domain/consensus/processes/transactionvalidator/transactionvalidator.go
@@ -4,10 +4,19 @@ import (
 	"github.com/zuanet/zuad/domain/consensus/model"
 	"github.com/zuanet/zuad/domain/consensus/model/externalapi"
 	"github.com/zuanet/zuad/domain/consensus/utils/txscript"
+	"github.com/zuanet/zuad/domain/consensus/vprog" 
 	"github.com/zuanet/zuad/util/txmass"
 )
 
-const sigCacheSize = 10_000
+const (
+	sigCacheSize = 10_000
+	// Tambah constant untuk vprog
+	maxVProgCodeSize        = 100_000 // 100KB max code size
+	maxVProgDataSize        = 50_000  // 50KB max input data
+	maxVProgGasLimit        = 1_000_000 // Max gas per transaction
+	maxVProgTotalGas        = 10_000_000 // Max gas per block
+	supportedVProgVersion   = 1
+)
 
 // transactionValidator exposes a set of validation classes, after which
 // it's possible to determine whether either a transaction is valid
@@ -24,10 +33,19 @@ type transactionValidator struct {
 	sigCache                                *txscript.SigCache
 	sigCacheECDSA                           *txscript.SigCacheECDSA
 	txMassCalculator                        *txmass.Calculator
+	
+	// TAMBAH FIELD UNTUK VPROG
+	vprogEngine                             *vprog.Engine
+	maxVProgCodeSize                        uint64
+	maxVProgDataSize                        uint64
+	maxVProgGasLimit                        uint64
+	maxVProgTotalGas                        uint64
+	supportedVProgVersion                   byte
 }
 
 // New instantiates a new TransactionValidator
-func New(blockCoinbaseMaturity uint64,
+func New(
+	blockCoinbaseMaturity uint64,
 	enableNonNativeSubnetworks bool,
 	maxCoinbasePayloadLength uint64,
 	ghostdagK externalapi.KType,
@@ -36,7 +54,11 @@ func New(blockCoinbaseMaturity uint64,
 	pastMedianTimeManager model.PastMedianTimeManager,
 	ghostdagDataStore model.GHOSTDAGDataStore,
 	daaBlocksStore model.DAABlocksStore,
-	txMassCalculator *txmass.Calculator) model.TransactionValidator {
+	txMassCalculator *txmass.Calculator,
+) model.TransactionValidator {
+
+	// Buat vprog engine
+	vprogEngine := vprog.NewEngine(databaseContext)
 
 	return &transactionValidator{
 		blockCoinbaseMaturity:                   blockCoinbaseMaturity,
@@ -51,5 +73,13 @@ func New(blockCoinbaseMaturity uint64,
 		sigCache:                                txscript.NewSigCache(sigCacheSize),
 		sigCacheECDSA:                           txscript.NewSigCacheECDSA(sigCacheSize),
 		txMassCalculator:                        txMassCalculator,
+		
+		// Inisialisasi vprog
+		vprogEngine:           vprogEngine,
+		maxVProgCodeSize:      maxVProgCodeSize,
+		maxVProgDataSize:      maxVProgDataSize,
+		maxVProgGasLimit:      maxVProgGasLimit,
+		maxVProgTotalGas:      maxVProgTotalGas,
+		supportedVProgVersion: supportedVProgVersion,
 	}
 }
